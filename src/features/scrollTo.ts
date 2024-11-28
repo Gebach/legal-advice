c1cc886fc6db8bd930331c6d52de76861ab926e7
Rodion <swizi_hs@mail.ru>
2024-11-28 23:02:36 +0500

    deploy

--- src/features/scrollTo.ts
+++
@@ -1,6 +0,0 @@
-export default function scrollTo(id: string) {
-  const element = document.querySelector(id)
-  if (element) {
-    element.scrollIntoView({ block: 'center', behavior: 'smooth' })
-  }
-}
--- src/shared/ui/AbsoluteContainer/AbsoulteContainer.tsx
+++
@@ -1,7 +0,0 @@
-import { AbsoulteContainerProps } from '../../model/types'
-
-function AbsoulteContainer({ children, classname }: AbsoulteContainerProps) {
-  return <div className={`md:absolute ${classname ?? ''}`}>{children}</div>
-}
-
-export default AbsoulteContainer
